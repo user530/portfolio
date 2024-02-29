14bad1205ea66eb9e622d7b94a68a023e02827c9
user530 <m.timarsuev@gmail.com>
2024-03-01 00:43:12 +0300

    Navbar component implemented

---
+++ src/components/navigation/Nav_bar.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styles from './Nav_bar.module.scss';
+import { FaCat } from "react-icons/fa";
+
+export const Navbar: React.FC = () => {
+    return (
+        <header className={styles['header']}>
+            <div className={styles['container']}>
+                <div className={styles['header__content']}>
+                    <a href='/' className={styles['logo']}>
+                        <FaCat className={styles['logo__icon']} />
+                        <span className={styles['logo__text']}>TMV</span>
+                    </a>
+
+                    <nav className={styles['nav-bar']}>
+                        <a className={styles['nav-bar__btn']} href="#!">Portfolio</a>
+                        <a className={styles['nav-bar__btn']} href="#!">About</a>
+                        <a className={styles['nav-bar__btn']} href="https://github.com/user530">GitHub</a>
+                    </nav>
+
+                    <button className={styles['contact-btn']}>
+                        Contact
+                    </button>
+                </div>
+            </div>
+        </header>
+    )
+}
