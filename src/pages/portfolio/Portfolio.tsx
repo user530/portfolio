897816892d12ec3f58696e71ec43276dd87b95eb
user530 <m.timarsuev@gmail.com>
2024-03-01 23:59:39 +0300

    Portfolio filter

--- src/pages/portfolio/Portfolio.tsx
+++ src/pages/portfolio/Portfolio.tsx
@@ -4,19 +4,36 @@ import styles from './Portfolio.module.scss';
 export const PortfolioPage: React.FC = () => {
     return (
         <>
-            <div className={styles['title']}>
+            <header className={styles['title']}>
                 <div className={styles['container']}>
                     <div className={styles['title__inner']}>
                         <h1 className={
                             [styles['h1'], styles['main-title']].join(' ')
                         }>Portfolio</h1>
                         <h3 className={
-                            [styles['h3'], styles['sub-title']].join(' ')
+                            [styles['text'], styles['sub-title']].join(' ')
                         }>Full stack web developer,<br />keeping up the grind, one step at a time</h3>
                     </div>
                 </div>
-            </div>
+            </header>
 
+            <main className={styles['content']}>
+                <div className={styles['container']}>
+                    <div className={styles['filter']}>
+                        <button className={
+                            [styles['filter__item'], styles['filter__item--active']].join(' ')
+                        }>
+                            React<span className={styles['item-counter']}>2</span>
+                        </button>
+                        <button className={styles['filter__item']}>
+                            Vue<span className={styles['item-counter']}>1</span></button>
+                        <button className={styles['filter__item']}>
+                            Express<span className={styles['item-counter']}>0</span></button>
+                        <button className={styles['filter__item']}>
+                            NestJs<span className={styles['item-counter']}>3</span></button>
+                    </div>
+                </div>
+            </main>
         </>
     )
 }
