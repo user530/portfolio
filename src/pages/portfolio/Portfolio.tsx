b343cd59bfe77dfae8f5e92a064acd3ad7994c2a
user530 <m.timarsuev@gmail.com>
2024-03-02 23:50:43 +0300

    Portfolio card block styles

--- src/pages/portfolio/Portfolio.tsx
+++ src/pages/portfolio/Portfolio.tsx
@@ -34,12 +34,87 @@ export const PortfolioPage: React.FC = () => {
                                 NestJs<span className={styles['item-counter']}>3</span></button>
                         </div>
 
-                        <div className={styles['cards']}>
-                            <div className={styles['card']}>CARD</div>
-                            <div className={styles['card']}>CARD</div>
-                            <div className={styles['card']}>CARD</div>
-                            <div className={styles['card']}>CARD</div>
-                            <div className={styles['card']}>CARD</div>
+                        <div className={styles['cards-grid']}>
+                            <a
+                                href='#!'
+                                target='_blank'
+                                className={styles['card']}>
+
+                                <div className={styles['card__info']}>
+                                    <ul className={styles['info__tags']}>
+                                        <li>React</li>
+                                        <li>NestJS</li>
+                                    </ul>
+                                    <h3 className={styles['info__title']}>Project Name</h3>
+                                </div>
+                                <div className={styles['card__details']}>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem repellat quia numquam voluptas doloremque fugiat ullam perferendis quisquam ipsum accusantium reprehenderit pariatur, sed voluptatibus! Praesentium minus ab beatae provident nesciunt.</p>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem repellat quia numquam voluptas doloremque fugiat ullam perferendis quisquam ipsum accusantium reprehenderit pariatur, sed voluptatibus! Praesentium minus ab beatae provident nesciunt.</p>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem repellat quia numquam voluptas doloremque fugiat ullam perferendis quisquam ipsum accusantium reprehenderit pariatur, sed voluptatibus! Praesentium minus ab beatae provident nesciunt.</p>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem repellat quia numquam voluptas doloremque fugiat ullam perferendis quisquam ipsum accusantium reprehenderit pariatur, sed voluptatibus! Praesentium minus ab beatae provident nesciunt.</p>
+                                </div>
+                            </a>
+
+                            <a
+                                href='#!'
+                                target='_blank'
+                                className={styles['card']}>
+
+                                <div className={styles['card__info']}>
+                                    <ul className={styles['info__tags']}>
+                                        <li>React</li>
+                                        <li>NestJS</li>
+                                    </ul>
+                                    <h3 className={styles['info__title']}>Project Name</h3>
+                                </div>
+                                <div className={styles['card__details']}>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem repellat quia numquam voluptas doloremque fugiat ullam perferendis quisquam ipsum accusantium reprehenderit pariatur, sed voluptatibus! Praesentium minus ab beatae provident nesciunt.</p>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem repellat quia numquam voluptas doloremque fugiat ullam perferendis quisquam ipsum accusantium reprehenderit pariatur, sed voluptatibus! Praesentium minus ab beatae provident nesciunt.</p>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem repellat quia numquam voluptas doloremque fugiat ullam perferendis quisquam ipsum accusantium reprehenderit pariatur, sed voluptatibus! Praesentium minus ab beatae provident nesciunt.</p>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem repellat quia numquam voluptas doloremque fugiat ullam perferendis quisquam ipsum accusantium reprehenderit pariatur, sed voluptatibus! Praesentium minus ab beatae provident nesciunt.</p>
+                                </div>
+                            </a>
+
+                            <a
+                                href='#!'
+                                target='_blank'
+                                className={styles['card']}>
+
+                                <div className={styles['card__info']}>
+                                    <ul className={styles['info__tags']}>
+                                        <li>React</li>
+                                        <li>NestJS</li>
+                                    </ul>
+                                    <h3 className={styles['info__title']}>Project Name</h3>
+                                </div>
+                                <div className={styles['card__details']}>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem repellat quia numquam voluptas doloremque fugiat ullam perferendis quisquam ipsum accusantium reprehenderit pariatur, sed voluptatibus! Praesentium minus ab beatae provident nesciunt.</p>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem repellat quia numquam voluptas doloremque fugiat ullam perferendis quisquam ipsum accusantium reprehenderit pariatur, sed voluptatibus! Praesentium minus ab beatae provident nesciunt.</p>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem repellat quia numquam voluptas doloremque fugiat ullam perferendis quisquam ipsum accusantium reprehenderit pariatur, sed voluptatibus! Praesentium minus ab beatae provident nesciunt.</p>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem repellat quia numquam voluptas doloremque fugiat ullam perferendis quisquam ipsum accusantium reprehenderit pariatur, sed voluptatibus! Praesentium minus ab beatae provident nesciunt.</p>
+                                </div>
+                            </a>
+
+                            <a
+                                href='#!'
+                                target='_blank'
+                                className={styles['card']}>
+
+                                <div className={styles['card__info']}>
+                                    <ul className={styles['info__tags']}>
+                                        <li>React</li>
+                                        <li>NestJS</li>
+                                    </ul>
+                                    <h3 className={styles['info__title']}>Project Name</h3>
+                                </div>
+                                <div className={styles['card__details']}>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem repellat quia numquam voluptas doloremque fugiat ullam perferendis quisquam ipsum accusantium reprehenderit pariatur, sed voluptatibus! Praesentium minus ab beatae provident nesciunt.</p>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem repellat quia numquam voluptas doloremque fugiat ullam perferendis quisquam ipsum accusantium reprehenderit pariatur, sed voluptatibus! Praesentium minus ab beatae provident nesciunt.</p>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem repellat quia numquam voluptas doloremque fugiat ullam perferendis quisquam ipsum accusantium reprehenderit pariatur, sed voluptatibus! Praesentium minus ab beatae provident nesciunt.</p>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem repellat quia numquam voluptas doloremque fugiat ullam perferendis quisquam ipsum accusantium reprehenderit pariatur, sed voluptatibus! Praesentium minus ab beatae provident nesciunt.</p>
+                                </div>
+                            </a>
+
                         </div>
                     </div>
                 </div>
