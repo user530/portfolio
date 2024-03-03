d4dc0780960155753d4a59bed00b3efcc3a2e3b6
user530 <m.timarsuev@gmail.com>
2024-03-03 23:13:11 +0300

    Nav bar mobile menu initial implementation

--- src/components/navigation/Nav_bar.tsx
+++ src/components/navigation/Nav_bar.tsx
@@ -1,8 +1,39 @@
 import React from 'react';
 import styles from './Nav_bar.module.scss';
 import { FaCat } from "react-icons/fa";
+import { MdMenu, MdOutlineClose } from "react-icons/md";
 
 export const Navbar: React.FC = () => {
+    const [width, setWidth] = React.useState<number>(window.innerWidth);
+    const [menuOpen, setMenuOpen] = React.useState<boolean>(false);
+    const burgerMenu = React.useRef<HTMLElement>(null);
+
+    const handleMenuClick = React.useCallback(() => {
+        setMenuOpen(prev => !prev);
+    }, [])
+
+    React.useEffect(
+        () => {
+            // Toggle body overflow
+            const bodyEl = document.body;
+            bodyEl.style.overflow = menuOpen === true ? 'hidden' : 'auto';
+
+            // Toggle class
+            burgerMenu.current?.classList.toggle(styles['nav-bar--open']);
+        },
+        [menuOpen]
+    )
+
+    React.useEffect(
+        () => {
+            const resizeHandler = () => setWidth(window.innerWidth);
+            window.addEventListener('resize', resizeHandler);
+
+            return () => window.removeEventListener('resize', resizeHandler);
+        },
+        []
+    )
+
     return (
         <header className={styles['header']}>
             <div className={styles['container']}>
@@ -12,15 +43,48 @@ export const Navbar: React.FC = () => {
                         <span className={styles['logo__text']}>TMV</span>
                     </a>
 
-                    <nav className={styles['nav-bar']}>
-                        <a className={styles['nav-bar__btn']} href="#portfolio">Portfolio</a>
-                        <a className={styles['nav-bar__btn']} href="#about" >About</a>
-                        <a className={styles['nav-bar__btn']} href="https://github.com/user530" target='_blank' rel="noreferrer" >GitHub</a>
+                    <nav
+                        className={styles['nav-bar']}
+                        ref={burgerMenu}
+                    >
+                        <a
+                            className={styles['nav-bar__btn']}
+                            href="#portfolio"
+                            onClick={handleMenuClick}
+                        >Portfolio</a>
+
+                        <a
+                            className={styles['nav-bar__btn']}
+                            href="#about"
+                            onClick={handleMenuClick}
+                        >About</a>
+
+                        <a
+                            className={styles['nav-bar__btn']}
+                            href="https://github.com/user530"
+                            target='_blank'
+                            rel="noreferrer"
+                            onClick={handleMenuClick}
+                        >GitHub</a>
                     </nav>
 
-                    <a href='mailto:m.timarsuev@gmail.com' className={styles['contact-btn']}>
-                        Contact
-                    </a>
+                    {
+                        width < 768
+                            ? <button
+                                className={styles['burger-menu']}
+                                onClick={handleMenuClick}
+                            >
+                                {
+                                    menuOpen ?
+                                        <MdOutlineClose />
+                                        : <MdMenu />
+                                }
+                            </button>
+                            : <a href='mailto:m.timarsuev@gmail.com' className={styles['contact-btn']}>
+                                Contact
+                            </a>
+                    }
+
                 </div>
             </div>
         </header>
