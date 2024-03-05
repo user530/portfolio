06f49b4668e0da142117f459de17a3c8b127794c
user530 <m.timarsuev@gmail.com>
2024-03-05 21:56:01 +0300

    Footer refactored

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,21 +1,17 @@
 import React from 'react';
 import styles from './Footer.module.scss';
-import { SocialIcons } from '../socialIcons/SocialIcons';
-import { FaWhatsapp, FaTelegram, FaLinkedin, FaPhoneAlt } from 'react-icons/fa';
+import { ISocialIcons, SocialIcons } from '../socialIcons/SocialIcons';
 
-const socialLinks = [
-    { icon: FaPhoneAlt, href: 'tel:+79271557757', },
-    { icon: FaWhatsapp, href: 'https://api.whatsapp.com/send?phone=79271557757', },
-    { icon: FaTelegram, href: 'https://t.me/@tmv95' },
-    { icon: FaLinkedin, href: 'www.linkedin.com/in/magomed-timarsuev-7249581aa', newTab: true },
-];
+export interface IFooter {
+    socialLinksProps: ISocialIcons;
+}
 
-export const Footer: React.FC = () => {
+export const Footer: React.FC<IFooter> = ({ socialLinksProps }: IFooter) => {
     return (
         <footer className={styles['footer']}>
             <div className={styles['container']}>
                 <div className={styles['footer__inner']}>
-                    <SocialIcons icons={socialLinks} />
+                    <SocialIcons {...socialLinksProps} />
                 </div>
             </div>
         </footer>
