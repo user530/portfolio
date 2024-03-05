0f8888256fd4de555822fd1d62741a5231c063b8
user530 <m.timarsuev@gmail.com>
2024-03-05 21:55:54 +0300

    Header refactored

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './Header.module.scss';
 import { useMediaQuery } from 'react-responsive';
-import { FaCat, FaLinkedin, FaPhoneAlt, FaTelegram, FaWhatsapp, FaEnvelope } from "react-icons/fa";
+import { FaEnvelope } from "react-icons/fa";
 import { ILogo, Logo } from '../logo/Logo';
 import { INavbar, Navbar } from '../navbar/Navbar';
 import { ContactBtn, IContactBtn } from '../contactBtn/ContactBtn';
@@ -9,67 +9,43 @@ import { MobMenuBtn } from '../mobMenuBtn/MobMenuBtn';
 import { MobMenu } from '../mobMenu/MobMenu';
 import { ISocialIcons } from '../socialIcons/SocialIcons';
 
-const navLinks = [
-    { name: 'Portfolio', href: '#portfolio' },
-    { name: 'About', href: '#about' },
-    { name: 'GitHub', href: 'https://github.com/user530', newTab: true },
-];
-
-const socialLinks = [
-    { icon: FaPhoneAlt, href: 'tel:+79271557757', },
-    { icon: FaWhatsapp, href: 'https://api.whatsapp.com/send?phone=79271557757', },
-    { icon: FaTelegram, href: 'https://t.me/@tmv95' },
-    { icon: FaLinkedin, href: 'www.linkedin.com/in/magomed-timarsuev-7249581aa', newTab: true },
-];
-
-export const Header: React.FC = () => {
-    const logoProps: ILogo = {
-        title: 'TMV',
-        link: '/',
-        icon: FaCat,
-    };
-
-    const navbarProps: INavbar = {
-        links: navLinks,
-    };
-
-    const contactBtnProps: IContactBtn = {
-        text: 'Contact',
-        href: 'mailto:m.timarsuev@gmail.com',
-    };
-
-    const socialIconsProps: ISocialIcons = {
-        icons: socialLinks,
-    };
-
-    return (
-        <header className={styles['header']}>
-            <div className={styles['container']}>
-                <div className={styles['header__content']}>
-
-                    <DefaultHeader
-                        logoProps={logoProps}
-                        navbarProps={navbarProps}
-                        contactBtnProps={contactBtnProps}
-                    />
-
-                    <MobileHeader
-                        logoProps={logoProps}
-                        navbarProps={navbarProps}
-                        socialIconsProps={
-                            {
-                                icons: [
-                                    { icon: FaEnvelope, href: contactBtnProps.href },
-                                    ...socialIconsProps.icons,
-                                ]
-                            }}
-                    />
-
-                </div>
-            </div>
-        </header>
-    )
+export interface IHeader {
+    logoProps: ILogo;
+    navbarProps: INavbar;
+    contactBtnProps: IContactBtn;
+    socialIconsProps: ISocialIcons;
 }
+
+export const Header: React.FC<IHeader> =
+    ({ logoProps, navbarProps, contactBtnProps, socialIconsProps }: IHeader) => {
+        return (
+            <header className={styles['header']}>
+                <div className={styles['container']}>
+                    <div className={styles['header__content']}>
+
+                        <DefaultHeader
+                            logoProps={logoProps}
+                            navbarProps={navbarProps}
+                            contactBtnProps={contactBtnProps}
+                        />
+
+                        <MobileHeader
+                            logoProps={logoProps}
+                            navbarProps={navbarProps}
+                            socialIconsProps={
+                                {
+                                    icons: [
+                                        { icon: FaEnvelope, href: contactBtnProps.href },
+                                        ...socialIconsProps.icons,
+                                    ]
+                                }}
+                        />
+
+                    </div>
+                </div>
+            </header>
+        )
+    }
 
 interface IMobileHeader {
     logoProps: ILogo;
